48589d3f7326d804fc8e8094be19a8450bcbf168
Nick Toledo <nickolasptoledo@gmail.com>
2022-07-21 13:24:58 -0300

    imc 1

---
+++ imc-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace imc_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu peso: " );
+            float peso = (float) Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite sua altura: ");
+            float altura = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite seu nome: ");
+           string nome = Console.ReadLine();
+
+
+            float imc = peso / (altura * altura);
+
+
+        }
+    }
+}
